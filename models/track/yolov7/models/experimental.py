6416aea65411148c6e478ff6594403002d846b0b
juye-ops <kjye.ops@gmail.com>
2023-01-27 09:03:55 +0000

    REFACTOR: Tracking의 Detect 함수화

--- models/track/yolov7/models/experimental.py
+++ models/track/yolov7/models/experimental.py
@@ -129,7 +129,7 @@ class Ensemble(nn.ModuleList):
 
 
 def attempt_load(weights, map_location=None, inplace=True):
-    from models.yolo import Detect, Model
+    from yolov7.models.yolo import Detect, Model
 
     # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
     model = Ensemble()
