133c9cc119838bb89f96e43368c5ac9603d6e78b
Gu Lee <99sphere@gmail.com>
2023-01-31 06:49:08 +0000

    FEAT: Remove log per image

--- models/track/yolov7/utils/datasets.py
+++ models/track/yolov7/utils/datasets.py
@@ -247,10 +247,10 @@ class LoadImages:  # for inference
                     ret_val, img0 = self.cap.read()
             img0 = resize_crop(img0)
             self.frame += 1
-            print(
-                f"video {self.count + 1}/{self.nf} ({self.frame}/{self.nframes}) {path}: ",
-                end="",
-            )
+            # print(
+            #     f"video {self.count + 1}/{self.nf} ({self.frame}/{self.nframes}) {path}: ",
+            #     end="",
+            # )
 
         else:
             # Read image
