4bd27e48f6dcebc20dee0b10035631c3d95b8100
Gu Lee <99sphere@gmail.com>
2023-02-01 02:45:23 +0000

    REFACTOR: split function 'calc_similarity' and 'dbscan' from 'mc_demo_yolov7.py' to 'utils.py'

--- models/track/tools/mc_demo_yolov7.py
+++ models/track/tools/mc_demo_yolov7.py
@@ -40,7 +40,9 @@ from tools.utils import (
     createDirectory,
     get_frame_num,
     bbox_scale_up,
-    calculate_similarity,
+    calc_similarity_v1,
+    calc_similarity_v2,
+    calc_similarity_v3,
     write_results,
 )
 from tools.mask_generator import (
@@ -54,113 +56,6 @@ from tools.mask_generator import (
 
 sys.path.insert(0, "./yolov7")
 sys.path.append(".")
-
-
-def dbscan(target_dir, tracklet_dir):
-    tracklet_imgs = glob.glob(tracklet_dir + "/*.png")
-    # encodings = [DeepFace.represent(img_path=img,enforce_detection=False,model_name="Facenet512") for img in tracklet_imgs]
-    data = []
-    for imagePath in tracklet_imgs:
-        image = cv2.imread(imagePath)
-        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        boxes = face_recognition.face_locations(rgb, model="cnn")
-        encodings = face_recognition.face_encodings(rgb, boxes)
-        d = [
-            {"imagePath": imagePath, "loc": box, "encoding": enc}
-            for (box, enc) in zip(boxes, encodings)
-        ]
-        data.extend(d)
-    encodings = [d["encoding"] for d in data]
-    # dump the facial encodings data to disk
-    stime = time.time()
-    clt = DBSCAN(metric="euclidean")
-    clt.fit(encodings)
-    etime = time.time()
-    label_ids = np.unique(clt.labels_)
-    numUniqueFaces = len(np.where(label_ids > -1)[0])
-    if opt.verbose:
-        print(f"DBSCAN time elapsed :{etime - stime}")
-        print("[INFO] # unique faces: {}".format(numUniqueFaces))
-
-def calc_similarity_v2(dfs, t_ids, tracklet_dir):
-
-    targeted_ids = {}
-
-    for i in range(len(dfs)):
-        id, sim = (
-            int(dfs.iloc[i].identity.split("/")[-1].split(".")[0]),
-            dfs.iloc[i]["VGG-Face_cosine"],
-        )
-        targeted_ids[id] = sim
-
-    targeted_ids = dict(sorted(targeted_ids.items(), key=lambda x: x[1]))
-    # t_ids = dict(sorted(t_ids.items(),key=lambda x : x[1],reverse=True))
-
-    best_matched_id = list(targeted_ids.keys())[0]
-
-    second_dfs = DeepFace.find(
-        img_path=f"{tracklet_dir}/{best_matched_id}.png",
-        db_path=tracklet_dir,
-        enforce_detection=False,
-        model_name="VGG-Face",
-    )
-    targeted_ids = {}
-    for i in range(len(second_dfs)):
-        id, sim = (
-            int(second_dfs.iloc[i].identity.split("/")[-1].split(".")[0]),
-            second_dfs.iloc[i]["VGG-Face_cosine"],
-        )
-        if sim < 0.25:
-            id_conf = t_ids.pop(id)
-            targeted_ids[id] = (id_conf, sim)
-
-    return targeted_ids, t_ids
-
-
-def calc_similarity_v3(dfs, t_ids, tracklet_dir):
-    sim_dict, sim_cnt = defaultdict(float), defaultdict(int)
-    best_matched_id = int(dfs.identity[0].split("/")[-1].split(".")[0])
-
-    second_dfs = DeepFace.find(
-        img_path=f"{tracklet_dir}/{best_matched_id}.png",
-        db_path=tracklet_dir,
-        enforce_detection=False,
-        model_name="VGG-Face",
-    )
-    cnt = 0
-    for i in range(len(second_dfs)):
-        second_id, second_sim = (
-            int(second_dfs.identity[i].split("/")[-1].split(".")[0]),
-            second_dfs["VGG-Face_cosine"][i],
-        )
-        if second_sim < 0.25:
-            cnt += 1
-            third_dfs = DeepFace.find(
-                img_path=f"{tracklet_dir}/{second_id}.png",
-                db_path=tracklet_dir,
-                enforce_detection=False,
-                model_name="VGG-Face",
-            )
-            for i in range(len(third_dfs)):
-                third_id, third_sim = (
-                    int(third_dfs.identity[i].split("/")[-1].split(".")[0]),
-                    third_dfs["VGG-Face_cosine"][i],
-                )
-                if third_sim < 0.25:
-                    sim_dict[third_id] += third_sim
-                    sim_cnt[third_id] += 1
-    for id in sim_dict.keys():
-        sim_dict[id] /= sim_cnt[id]
-
-    targeted_ids = {}
-
-    if sim_cnt:
-        for tid, t_cnt in sim_cnt.items():
-            if t_cnt / cnt >= 0.75:
-                targeted_ids[tid] = (t_ids.pop(tid), sim_dict[tid])
-
-    return targeted_ids, t_ids
-
 
 
 def get_valid_tids(tracker, results, tracklet_dir, target_dir, min_length, conf_thresh):
